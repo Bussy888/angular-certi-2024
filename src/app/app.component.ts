bd2545f3c934812a418d6c018a9542dafd3b8f5e
null <null>
2024-09-26 19:39:33 -0400

    [25-09-2024] nullish coalescing, OR, 1st component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,72 +1,95 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserCardComponent } from './user-card/user-card.component';
 
-interface IPerson{
-  name:string
-  lastname:string
-  age?:number
+interface IPerson {
+  name: string
+  lastName: string
+  age?: number
 }
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title:number = 20;
-  animals:string[]=['a','b','c','d','e']
- 
+  title: number = 10;
+  animals: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 
-  person: IPerson={
-    name:'Juan',
-    lastname:'Perez',
+  person: IPerson = {
+    name: 'Juan',
+    lastName: 'Perez',
     age: 25
-  } 
-  students:number[]=[1,2,3,4,5]
-  parents:number[]=[6,7,8,9,10]
-  constructor(){
-    const {name, age} = this.person
-    console.log("desestructuracion: ", name, age)
+  }
+  students:number[] = [1,2,3,4,5,6]
+  parents:number[] = [7,8,9,10]
 
+  var1 = 0
+  var2 = null
+  var3 = 'hola'
+
+  constructor() {
+    const { name, age } = this.person
+    console.log('desestructuracion: ', name, age)
     let both = [...this.students, ...this.parents]
-    console.log("spreed operator: ", both)
+    console.log('spreed operator:', both)
+    console.log('REST operator:', this.sum(2,4,6))
 
-    console.log('REST Operator: ', this.sum(2,4,6))
-    
-    console.log('REST Operator with reduce: ', this.sum2(2,4,6))
+    console.log('Nullish Coalesing:',  this.var2 ?? this.var3  )
+    console.log('OR:', this.var2 || this.var1)
 
-    // console.log('substract ', this.substract(8,4))
-    // console.log('Map: ',this.animals.map((animal) => (animal + 'new')))
-    // console.log('Foreach: ',this.animals.forEach((animal) => (animal + 'new')))
-    // console.log('Find: ',this.animals.find((animal)=>animal==='c'))
-    // console.log('Filter: ',this.animals.filter((animal)=>animal==='c'))
-    // console.log('IndexOF: ',this.animals.indexOf('c'))
+
+    //console.log('subtract ', this.subtract(8, 4))
+    //console.log('MAP:', this.animals.map((animal: string) => (animal + 'new')))
+    //console.log('FOREACH:', this.animals.forEach((animal) => (animal + 'new')))
+    //console.log('FIND', this.animals.find((animal) => animal === 'z'))
+    //console.log('FILTER', this.animals.filter((animal) => animal === 'z'))
+    //console.log('INDEXOF', this.animals.indexOf('c'))
   }
   public sum(...persons:number[]){
-    return persons[0]+persons[1]
+    //return persons[0] + persons[1]
+    return persons.reduce(
+      (acumulador, valorActual) => (acumulador + valorActual), 10
+    )
   }
-  public sum2(...persons:number[]){
-    return persons.reduce((acumulador, valorActual)=> (acumulador+valorActual), 0)
+
+  public sum2(num1: number, num2: number): number {
+    return num1 + num2
   }
-  public sum3(num1: number, num2:number): number{
-    return num1+num2
+
+  private subtract(num1: number, num2: number): number {
+    return num1 - num2
   }
-  public substract(num1: number, num2:number): number{
-    return num1-num2
-  }
+
   public getArray(): void {
-    const persons: number[] = [1, 2, 3, 4, 5];
+    const persons: number[] = [1, 2, 3, 4, 5]
     for (let i = 0; i < persons.length; i++) {
-        if (persons[i] % 2 === 0) {
-            console.log('person =', persons[i]);
-        }
+      //console.log('person =', persons[i])
     }
-}
-//const sumar=() => {
-//   return 1+2;
-// }
-// const resta =()=>'hola'
-  
+  }
+
+
+
+  //  function sumar(){
+  //   return 1 + 2;
+  //  }  
+
+  //  const suma = () => {
+  //   return 1 + 2
+  //  }
+
+
+  //  function resta(){
+  //   return 'hola' + a
+  //  }
+
+  // arrow functions
+  //  const resta = () => ('hola' + a)
+
+
 
 }
