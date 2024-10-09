2ad60994ead51e175841d4907a6fcbb1665dede6
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2024-10-09 16:32:03 -0400

    [08-10-2024] Modules and components for pre-test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductModule } from './product/product.module';
+import { ClientModule } from './client/client.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductModule, ClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 20;
+  title = 'nuevo_proyecto_angular';
 }
---
+++ src/app/client/provider/provider2/provider2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provider2',
+  templateUrl: './provider2.component.html',
+  styleUrl: './provider2.component.scss'
+})
+export class Provider2Component {
+
+}
+
