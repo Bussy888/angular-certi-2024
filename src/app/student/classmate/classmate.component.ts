78b580b75056b3beaf7a173be049a4cf7f1f71f3
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2024-10-17 20:20:41 -0400

    [17-10-2024] navigation from html

--- src/app/student/classmate/classmate.component.ts
+++ src/app/student/classmate/classmate.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-classmate',
-  standalone: true,
-  imports: [],
   templateUrl: './classmate.component.html',
   styleUrl: './classmate.component.scss'
 })
