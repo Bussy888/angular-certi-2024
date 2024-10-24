a57f5f8852b13a2c2bf4170a6f6097a8144291a9
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2024-10-23 21:17:33 -0400

    [23-10-2024] validations in template driven form and reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -128,6 +128,10 @@ export class AppComponent {
     })
   }
 
+  print(){
+    console.log('FORM NAME: ', this.studentForm.get('name'))
+  }
+
   onSendData() {
     console.log('FORM GROUP: ', this.studentForm)
   }
