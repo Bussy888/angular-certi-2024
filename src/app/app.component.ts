716d881eb6059fd14af9e018feba637a01aebdab
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2024-10-31 21:22:24 -0400

    [31-10-2022] Http service GET

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { ImpurePipe } from "./impure.pipe";
 import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from "@angular/material/button";
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from "@angular/forms";
+import { StudentService } from "./services/student.service";
 
 interface IPerson {
   name: string;
@@ -84,9 +85,16 @@ export class AppComponent {
   student2Form!: UntypedFormGroup
 
   constructor(
+    private _studentService: StudentService,
     private router: Router, 
     private formBuilder: FormBuilder,
     private untypedFormBuilder: UntypedFormBuilder) {
+
+    this._studentService.getStudents().subscribe((res) => {
+      console.log('STUDENTS JSON: ', res)
+    });  
+
+
     const { name, age } = this.person;
     let both = [...this.students, ...this.parents];
 
