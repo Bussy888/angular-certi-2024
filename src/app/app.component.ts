827de2a22bbb157c7761eaa6c1726c4917b9d1a2
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2024-11-05 20:45:27 -0400

    [05-11-2024] Final Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CityListComponent } from './city-list/city-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CityListComponent, HttpClientModule, FormsModule],  // Añadido HttpClientModule y FormsModule
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 20;
+  title = 'Examen Final - Mateo Michel'; 
 }
